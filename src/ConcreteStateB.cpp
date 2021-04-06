ffe00d06c78a1653ee538ed3ec253fb50e05f0c8
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-04-06 22:37:42 +0200

    Create ConcreteStateB.cpp

---
+++ src/ConcreteStateB.cpp
@@ -0,0 +1,15 @@
+#include "ConcreteStateA.h"
+#include "ConcreteStateB.h"
+#include "Context.h"
+
+
+void ConcreteStateB::Handle1()
+{
+  std::cout << "ConcreteStateB: request1.\n";
+}
+void ConcreteStateB::Handle2()
+{
+  std::cout << "ConcreteStateB: request2.\n";
+  std::cout << "ConcreteStateB: trying to change the state of the context.\n";
+  this->context_->TransitionTo(new ConcreteStateA);
+}
