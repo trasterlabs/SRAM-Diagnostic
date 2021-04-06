d3b5484257585e33560b7baf53a1ccae8e5961e9
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-04-06 22:36:16 +0200

    Create context.cpp

---
+++ src/context.cpp
@@ -0,0 +1,30 @@
+#include "Context.h"
+#include <typeinfo>
+
+Context::Context (State *state) : state_(nullptr)
+{
+  this->TransitionTo(state);
+}
+Context::~Context ( void )
+{
+  delete state_;
+}
+
+void Context::TransitionTo (State *state)
+{
+  std::cout << "Context: Transition to " << typeid(*state).name() << ".\n";
+  if (this->state_ != nullptr)
+  {
+    delete this->state_;
+  }
+  this->state_ = state;
+  this->state_->set_context(this);
+}
+void Context::Request1 ( void )
+{
+  this->state_->Handle1();
+}
+void Context::Request2 ( void )
+{
+  this->state_->Handle2();
+}
