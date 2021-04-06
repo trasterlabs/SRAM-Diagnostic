a5129e4a159ecf51b87891dd553a5cdc87572f92
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-04-06 22:33:44 +0200

    Create context.h

---
+++ context.h
@@ -0,0 +1,34 @@
+#ifndef CONTEXT_H
+#define CONTEXT_H
+
+#include <iostream>
+#include "state.h"
+
+
+/**
+ * The Context defines the interface of interest to clients. It also maintains a
+ * reference to an instance of a State subclass, which represents the current
+ * state of the Context.
+ */
+class Context {
+  /**
+   * @var State A reference to the current state of the Context.
+   */
+ private:
+  State *state_;
+
+ public:
+  Context(State *state);
+  ~Context();
+  /**
+   * The Context allows changing the State object at runtime.
+   */
+  void TransitionTo(State *state);
+  /**
+   * The Context delegates part of its behavior to the current State object.
+   */
+  void Request1();
+  void Request2();
+};
+
+#endif // CONTEXT_H
