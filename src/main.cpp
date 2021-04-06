580b619e90c29b7ae558bbac1ca8744d20753e36
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-04-06 23:19:11 +0200

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Context.h"
+#include "ConcreteStateA.h"
+
+/**
+ * The client code.
+ */
+void ClientCode()
+{
+  Context *context = new Context(new ConcreteStateA);
+  context->Request1();
+  context->Request2();
+  delete context;
+}
+
+int main ( void )
+{
+  ClientCode();
+  return 0;
+}
