38a62b14a5ff97652a4e3cbeb528d77070000c8e
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-04-06 22:38:22 +0200

    Create ConcreteStateA.h

---
+++ include/ConcreteStateA.h
@@ -0,0 +1,19 @@
+#ifndef CONCRETESTATEA_H
+#define CONCRETESTATEA_H
+
+#include "state.h"
+
+/**
+ * Concrete States implement various behaviors, associated with a state of the
+ * Context.
+ */
+
+class ConcreteStateA : public State
+{
+ public:
+  void Handle1() override;
+
+  void Handle2() override;
+};
+
+#endif // CONCRETESTATEA_H
