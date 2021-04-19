61d3d4f8c1973b70e09e8d40ebc18eb2324af67a
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-04-19 22:24:42 +0200

    Searching the gcda files

--- create_coverage.sh
+++ create_coverage.sh
@@ -5,11 +5,18 @@ if [ ! -d ./cov/ ]; then mkdir -p ./cov/; fi
 if [ ! -d ./gcov/ ]; then mkdir -p ./gcov/; fi
 if [ ! -d ./gcov/aux ]; then mkdir -p ./gcov/aux; fi
 
-# me voy de paseo a la carpeta auxiliar
 cd ./gcov/aux
 
+# searching files with gcno extension
 files_with_gcno=$(find ../../ -type f "*.gcno")
 for each_file in $files_with_gcno
+do
+     echo "$each_file"
+done
+
+# searching files with gcda extension
+files_with_gcda=$(find ../../ -type f "*.gcda")
+for each_file in $files_with_gcda
 do
      echo "$each_file"
 done
