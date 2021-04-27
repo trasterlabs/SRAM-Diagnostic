62bf144e1ce5cde2b7464a83f945b7a9a2cf3a22
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-04-27 22:41:43 +0200

    Update test_MemPoolTemplate.cpp

--- test/test_MemPoolTemplate.cpp
+++ test/test_MemPoolTemplate.cpp
@@ -10,3 +10,41 @@
  * | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |  0 | <- Cuántos objetos hay seguidos
  * +---+---+---+---+---+---+---+---+---+----+
  */
+
+#include "gtest/gtest.h"
+#include "StaticMemoryPool.hpp"
+
+#define NELMS 10
+
+typedef MemoryPool<int, NELMS> intPool;
+
+class MemPoolTestingFixture : public testing::Test
+{
+protected:
+  void SetUp () override
+  {
+    pool_elements = intPool::ReceiveThePool();
+    pool_elements_used = intPool::ReceiveElementsUsed();
+    for ( int i = 0; i < NELMS; i++ )
+    {
+      pool_elements[i] = 0;
+    }
+    for ( int i = 0; i < NELMS; i++ )
+    {
+      pool_elements_used[i] = 0;
+    }
+  }
+  
+  intPool the_pool;
+  int * pool_elements;
+  uint8_t * pool_elements_used;
+};
+
+TEST_F(QueueTestSmpl3, ConteoDeMaximos01)
+{
+  //Arrange
+  pool_elements_used[3] = 1;
+  pool_elements_used[7] = 1;
+  //Act
+
+}
