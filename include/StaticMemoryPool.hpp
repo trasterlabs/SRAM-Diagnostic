1049470f9e7ec44e7399ba7f1ef82c8bb6f3e6fe
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-04-27 22:15:05 +0200

    Added the access to internals only for testing.

--- include/StaticMemoryPool.hpp
+++ include/StaticMemoryPool.hpp
@@ -62,6 +62,16 @@ public:
   bool isPositionInsideMemoryPool (unsigned int position);
   unsigned int firstAvailableBuffer (unsigned int size_of_the_desired_buffer);
   void showTheElementsUsed ();
+  #ifdef GOOGLETEST_INCLUDE_GTEST_GTEST_H_
+  theType * ReceiveThePool ()
+  {
+    return &(this->the_pool[0]);
+  }
+  uint8_t * ReceiveElementsUsed ()
+  {
+    return &(this->elements_used[0]);
+  }
+  #endif
 };
 
 /**
