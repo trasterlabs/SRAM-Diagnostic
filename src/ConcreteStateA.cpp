d60a93b419bf78cb2f68f33d74bab5ed2c6ecf06
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-05-18 22:38:49 +0200

    Update ConcreteStateA.cpp

--- src/ConcreteStateA.cpp
+++ src/ConcreteStateA.cpp
@@ -3,6 +3,34 @@
 #include "context.h"
 #include <iostream>
 
+#include "StaticMemoryPool.h"
+
+typedef MemoryPool<ConcreteStateA,3> concreteStateAPool;
+
+void* ConcreteStateA::operator new(size_t sz)
+{
+  concreteStateAPool & memorypool = concreteStateAPool::GetInstance();
+  return memorypool.new_();
+}
+
+void* ConcreteStateA::operator new[](size_t sz)
+{
+  concreteStateAPool & memorypool = concreteStateAPool::GetInstance();
+  return memorypool.new_( sz / sizeof ( ConcreteStateA ) );
+}
+
+void ConcreteStateA::operator delete(void* ptr)
+{
+  concreteStateAPool & memorypool = concreteStateAPool::GetInstance();
+  return memorypool.delete_( static_cast <ConcreteStateA *> (ptr) );
+}
+
+void ConcreteStateA::operator delete[](void* ptr)
+{
+  concreteStateAPool & memorypool = concreteStateAPool::GetInstance();
+  return memorypool.delete_( static_cast <ConcreteStateA *> (ptr) );
+}
+
 void ConcreteStateA::Handle1()
 {
   std::cout << "ConcreteStateA: request1.\n";
