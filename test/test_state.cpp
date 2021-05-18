126c93f892c02549ee302759cf09969d5f324aa8
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-05-18 22:02:23 +0200

    Update test_state.cpp

--- test/test_state.cpp
+++ test/test_state.cpp
@@ -5,6 +5,7 @@
 #include "context.h"
 #include "ConcreteStateA.h"
 
+#if 0
 TEST(Methods_random, Basic_functionality)
 {
   Context *context = new Context(new ConcreteStateA);
@@ -14,4 +15,15 @@ TEST(Methods_random, Basic_functionality)
   context->Request2();
   context->Request1();
   delete context;
+}
+#endif
+
+TEST(Screen_functionality, Screen01_test)
+{
+  //Arrange
+  Context *context = new Context(new ConcreteStateA);
+  //Act
+  const string * prueba = context->ShowScreen();
+  //Assert
+  EXPECT_STREQ( prueba, "=========\n Todo_OK \n=========\n" );
 }
