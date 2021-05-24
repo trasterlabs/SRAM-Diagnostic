1a30156694dcda5a9d9c27538361c1b7a43e35a1
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-05-24 22:03:10 +0200

    Update StaticMemoryPool.hpp

--- include/StaticMemoryPool.hpp
+++ include/StaticMemoryPool.hpp
@@ -11,6 +11,7 @@
   ******************************************************************************
   */
 
+#include <stdint.h>
 
 template <typename theType, unsigned int numberOfElements>
 class MemoryPool
