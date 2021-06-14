7bbc13901856df2f3c6b509662f4732bba369afa
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-06-14 22:35:30 +0200

    Update Key.cpp

--- src/Key.cpp
+++ src/Key.cpp
@@ -1,5 +1,23 @@
+/**
+  ******************************************************************************
+  * @file    Key.cpp
+  * @author  Trasterlabs
+  * @brief   Where the FSM of the key behaviour takes place.
+  ******************************************************************************
+  * @attention
+  *
+  * License Creative Commons CC BY-NC-SA 3.0
+  *
+  ******************************************************************************
+  */
+
 #include "Key.h"
 
+/**
+  * @brief  Constructor de la "tecla": utiliza un estado de partida.
+  * @param  [in] state El estado de entrada definido por las clases derivadas de KeyState. Por defecto, se introduce el estado NULL
+  * @see KeyState
+  */
 Key::Key (KeyState  *state) : state_(nullptr)
 {
   this->TransitionTo(state);
@@ -9,6 +27,11 @@ Key::~Key ( void )
   delete state_;
 }
 
+/**
+  * @brief  "La transición a" o "transicionar al" es la método que permite saltar de un estado al estado siguiente
+  * @param  [in] state El estado de entrada definido por las clases derivadas de KeyState. Por defecto, se introduce el estado NULL
+  * @see KeyState
+  */
 void Key::TransitionTo (KeyState *state)
 {
   if (this->state_ != nullptr)
@@ -16,8 +39,9 @@ void Key::TransitionTo (KeyState *state)
     delete this->state_;
   }
   this->state_ = state;
-    this->state_->set_Key(this);
+  this->state_->set_Key(this);
 }
+
 void Key::Open ( void )
 {
   this->state_->Open();
