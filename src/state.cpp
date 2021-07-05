d351cb84422a224238c5a3dee6396f9cd30aa764
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-07-05 22:20:01 +0200

    Update state.cpp

--- src/state.cpp
+++ src/state.cpp
@@ -1,5 +1,31 @@
 #include "state.h"
 
+typedef MemoryPool<State,3> StatePool;
+
+void* State::operator new(size_t sz)
+{
+  StatePool & memorypool = StatePool::GetInstance();
+  return memorypool.new_();
+}
+
+void* State::operator new[](size_t sz)
+{
+  StatePool & memorypool = StatePool::GetInstance();
+  return memorypool.new_( sz / sizeof ( State ) );
+}
+
+void State::operator delete(void* ptr)
+{
+  StatePool & memorypool = StatePool::GetInstance();
+  return memorypool.delete_( static_cast <State *> (ptr) );
+}
+
+void State::operator delete[](void* ptr)
+{
+  StatePool & memorypool = StatePool::GetInstance();
+  return memorypool.delete_( static_cast <State *> (ptr) );
+}
+
 void State::set_context(Context *context)
 {
   this->context_ = context;
