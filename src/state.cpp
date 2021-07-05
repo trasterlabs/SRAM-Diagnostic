cef131d670e07e005062091964f890744e2d2182
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-07-05 22:39:04 +0200

    Deleted the memorypool boilerplate

--- src/state.cpp
+++ src/state.cpp
@@ -1,31 +1,4 @@
-#include "StaticMemoryPool.hpp"
 #include "state.h"
-
-typedef MemoryPool<State,3> StatePool;
-
-void* State::operator new(size_t sz)
-{
-  StatePool & memorypool = StatePool::GetInstance();
-  return memorypool.new_();
-}
-
-void* State::operator new[](size_t sz)
-{
-  StatePool & memorypool = StatePool::GetInstance();
-  return memorypool.new_( sz / sizeof ( State ) );
-}
-
-void State::operator delete(void* ptr)
-{
-  StatePool & memorypool = StatePool::GetInstance();
-  return memorypool.delete_( static_cast <State *> (ptr) );
-}
-
-void State::operator delete[](void* ptr)
-{
-  StatePool & memorypool = StatePool::GetInstance();
-  return memorypool.delete_( static_cast <State *> (ptr) );
-}
 
 void State::set_context(Context *context)
 {
